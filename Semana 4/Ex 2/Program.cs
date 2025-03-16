85a8652494cfd8a4c215904af028947050c62d67
Paulo Henrique Barbosa Botelho <70411064+Paulo-Botelho@users.noreply.github.com>
2025-03-16 18:37:33 -0300

    semana 4

---
+++ Semana 4/Ex 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    static void Main(string[] args)
+    {
+        Pessoa obj = new Pessoa();
+        obj.nome = "Paulo";
+        obj.idade = 26;
+        obj.mensagem();
+    }
+}
